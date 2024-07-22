f821d5ad1d9e83084f809b41ffde4e811ed47280
myusername <andonyc2@icloud.com>
2024-07-21 19:37:39 -0600

    commit by andony calvo conexion BD

---
+++ src/main/java/BD/ConexionOracle.java
@@ -0,0 +1,16 @@
+
+package BD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionOracle {
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe"; 
+    private static final String USER = "ANDONY";
+    private static final String PASSWORD = "Colon1289";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
