6c9697e5d9f076ae4cd7c140c0c06a2d3f745cd7
andony89 <74324817+andony89@users.noreply.github.com>
2024-08-26 18:59:40 -0600

    Update Cursores_ProyectoFinal.sql

--- Cursores_ProyectoFinal.sql
+++ Cursores_ProyectoFinal.sql
@@ -191,3 +191,216 @@ BEGIN
 END ObtenerVentasPorFecha;
 /
 
+
+
+------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION ObtenerEmpleados
+RETURN SYS_REFCURSOR IS
+    empleados SYS_REFCURSOR;
+BEGIN
+    OPEN empleados FOR
+        SELECT EmpleadoID, Nombre, Telefono, Email, Posicion, FechaContratacion, Salario
+        FROM Empleados;
+    RETURN empleados;
+END ObtenerEmpleados;
+/
+    
+----------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerCategorias
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT CategoriaID, Nombre, Descripcion
+        FROM Categorias;
+    RETURN p_cursor;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error al obtener las categorías: ' || SQLERRM);
+        IF p_cursor%ISOPEN THEN
+            CLOSE p_cursor;
+        END IF;
+        RETURN NULL;
+END ObtenerCategorias;
+/
+    
+----------------------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerProveedores
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT ProveedorID, Nombre, Telefono, Direccion, Email
+        FROM Proveedores;
+    RETURN p_cursor;
+END ObtenerProveedores;
+/
+    
+------------------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerClientes
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT CedulaID, Nombre, Telefono, Direccion, Email
+        FROM Clientes;
+    RETURN p_cursor;
+END ObtenerClientes;
+/
+    
+------------------------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerProductos
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT ProductoID, Nombre, CategoriaID, Precio, Slock, ProveedorID
+        FROM Productos;
+    RETURN p_cursor;
+END ObtenerProductos;
+/
+    
+--------------------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerInventario
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT InventarioID, EmpleadoID, ProductoID, Cantidad, FechaActualizada
+        FROM Inventario;
+    RETURN p_cursor;
+END ObtenerInventario;
+/
+    
+---------------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerOrdenesCompra
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT OrdenCompraID, Fecha, ProveedorID, Total, Estado
+        FROM OrdenesCompra;
+    RETURN p_cursor;
+END ObtenerOrdenesCompra;
+/
+    
+--------------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerVentas
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT VentaID, Fecha, CedulaID, Total
+        FROM Ventas;
+    RETURN p_cursor;
+END ObtenerVentas;
+/
+    
+--------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerFactura
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT FacturaID, VentaID, ProductoID, Cantidad, PrecioUnitario, PrecioTotal
+        FROM Factura;
+    RETURN p_cursor;
+END ObtenerFactura;
+/
+    
+-----------------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerHistoricoOrdenesCompra
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT HistoricoID, OrdenCompraID, ProductoID, Cantidad, Estado
+        FROM HistoricoOrdenesCompra;
+    RETURN p_cursor;
+END ObtenerHistoricoOrdenesCompra;
+/
+    
+----------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerEmpleadosPorPosicion(p_Posicion VARCHAR2)
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT EmpleadoID, Nombre, Telefono, Email, Posicion, FechaContratacion, Salario
+        FROM Empleados
+        WHERE Posicion = p_Posicion;
+    RETURN p_cursor;
+END ObtenerEmpleadosPorPosicion;
+/
+    
+--------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerEmpleadosPorSalario(p_SalarioMinimo NUMBER)
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT EmpleadoID, Nombre, Telefono, Email, Posicion, FechaContratacion, Salario
+        FROM Empleados
+        WHERE Salario > p_SalarioMinimo;
+    RETURN p_cursor;
+END ObtenerEmpleadosPorSalario;
+/
+    
+------------------------------------------------------------------
+    
+Copiar código
+CREATE OR REPLACE FUNCTION ObtenerProductosPorCategoria(p_CategoriaNombre VARCHAR2)
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT p.ProductoID, p.Nombre, p.Precio, p.Slock, p.ProveedorID
+        FROM Productos p
+        JOIN Categorias c ON p.CategoriaID = c.CategoriaID
+        WHERE c.Nombre = p_CategoriaNombre;
+    RETURN p_cursor;
+END ObtenerProductosPorCategoria;
+/
+    
+---------------------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerOrdenesCompraPorEstado(p_Estado VARCHAR2)
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT OrdenCompraID, Fecha, ProveedorID, Total
+        FROM OrdenesCompra
+        WHERE Estado = p_Estado;
+    RETURN p_cursor;
+END ObtenerOrdenesCompraPorEstado;
+/
+
+---------------------------------------------
+    
+CREATE OR REPLACE FUNCTION ObtenerVentasPorFecha(p_FechaInicio DATE, p_FechaFin DATE)
+RETURN SYS_REFCURSOR IS
+    p_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN p_cursor FOR
+        SELECT VentaID, Fecha, CedulaID, Total
+        FROM Ventas
+        WHERE Fecha BETWEEN p_FechaInicio AND p_FechaFin;
+    RETURN p_cursor;
+END ObtenerVentasPorFecha;
+/
+
+
+
